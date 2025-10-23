ad8a411c490a1804e557f2b7ba4d5ac5cd62718b
Alex Botha <alex@byteheavy.com>
2025-10-23 10:27:47 +0200

    Test commit

--- public/index.php
+++ public/index.php
@@ -9,8 +9,9 @@ session_start();
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>🚨 CRIME REPORT SYSTEM</h1>
+    <h1> CRIME REPORT SYSTEM</h1>
     <p>Welcome to the PHP XAMPP Crime Reporting Project!</p>
+    <p>This is an example of using git</p>
     <p><strong>Server:</strong> <?php echo $_SERVER['SERVER_SOFTWARE']; ?></p>
 </body>
 </html>
